b02e447b525b0e7fa7d8b9713e6a3d75c57fad48
marinadimovska <mdimovska08@gmail.com>
2023-03-10 16:38:26 +0200

    MySQL-TU-Sofia

---
+++ LAB-3/ex1.sql
@@ -0,0 +1,315 @@
+DROP DATABASE IF EXISTS school_sport_clubs;
+CREATE DATABASE school_sport_clubs;
+USE school_sport_clubs;
+
+CREATE TABLE school_sport_clubs.sports(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE school_sport_clubs.coaches(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	name VARCHAR(255) NOT NULL ,
+	egn VARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE school_sport_clubs.students(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	name VARCHAR(255) NOT NULL ,
+	egn VARCHAR(10) NOT NULL UNIQUE ,
+	address VARCHAR(255) NOT NULL ,
+	phone VARCHAR(20) NULL DEFAULT NULL ,
+	class VARCHAR(10) NULL DEFAULT NULL   
+);
+
+CREATE TABLE school_sport_clubs.sportGroups(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	location VARCHAR(255) NOT NULL ,
+	dayOfWeek ENUM('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') ,
+	hourOfTraining TIME NOT NULL ,
+	sport_id INT NULL ,
+	coach_id INT NULL ,
+	UNIQUE KEY(location,dayOfWeek,hourOfTraining)  ,
+	CONSTRAINT FOREIGN KEY(sport_id) 
+		REFERENCES sports(id) ,
+	CONSTRAINT FOREIGN KEY (coach_id) 
+		REFERENCES coaches(id)
+);
+
+CREATE TABLE school_sport_clubs.student_sport(
+	student_id INT NOT NULL , 
+	sportGroup_id INT NOT NULL ,  
+	CONSTRAINT FOREIGN KEY (student_id) 
+		REFERENCES students(id) ,	
+	CONSTRAINT FOREIGN KEY (sportGroup_id) 
+		REFERENCES sportGroups(id) ,
+	PRIMARY KEY(student_id,sportGroup_id)
+);
+
+CREATE TABLE taxesPayments(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	student_id INT NOT NULL,
+	group_id INT NOT NULL,
+	paymentAmount DOUBLE NOT NULL,
+	month TINYINT,
+	year YEAR,
+	dateOfPayment DATETIME NOT NULL ,
+	CONSTRAINT FOREIGN KEY (student_id) 
+		REFERENCES students(id),
+	CONSTRAINT FOREIGN KEY (group_id) 
+		REFERENCES sportgroups(id)
+);
+
+CREATE TABLE salaryPayments(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	coach_id INT NOT NULL,
+	month TINYINT,
+	year YEAR,
+	salaryAmount double,
+	dateOfPayment datetime not null,
+	CONSTRAINT FOREIGN KEY (coach_id) 
+		REFERENCES coaches(id),
+	UNIQUE KEY(`coach_id`,`month`,`year`)
+);
+
+INSERT INTO sports
+VALUES 	(NULL, 'Football') ,
+		(NULL, 'Volleyball') ,
+		(NULL, 'Tennis') ,
+		(NULL, 'Karate') ,
+		(NULL, 'Taekwon-do');
+		
+INSERT INTO coaches  
+VALUES 	(NULL, 'Ivan Todorov Petkov', '7509041245') ,
+		(NULL, 'georgi Ivanov Todorov', '8010091245') ,
+		(NULL, 'Ilian Todorov Georgiev', '8407106352') ,
+		(NULL, 'Petar Slavkov Yordanov', '7010102045') ,
+		(NULL, 'Todor Ivanov Ivanov', '8302160980') , 
+		(NULL, 'Slavi Petkov Petkov', '7106041278');
+		
+INSERT INTO students (name, egn, address, phone, class) 
+VALUES 	('Iliyan Ivanov', '9401150045', 'Sofia-Mladost 1', '0893452120', '10') ,
+		('Ivan Iliev Georgiev', '9510104512', 'Sofia-Liylin', '0894123456', '11') ,
+		('Elena Petrova Petrova', '9505052154', 'Sofia-Mladost 3', '0897852412', '11') ,
+		('Ivan Iliev Iliev', '9510104542', 'Sofia-Mladost 3', '0894123457', '11') ,
+		('Maria Hristova Dimova', '9510104547', 'Sofia-Mladost 4', '0894123442', '11') ,
+		('Antoaneta Ivanova Georgieva', '9411104547', 'Sofia-Krasno selo', '0874526235', '10');
+		
+INSERT INTO sportGroups
+VALUES 	(NULL, 'Sofia-Mladost 1', 'Monday', '08:00:00', 1, 1 ) ,
+		(NULL, 'Sofia-Mladost 1', 'Monday', '09:30:00', 1, 2 ) ,
+		(NULL, 'Sofia-Liylin 7', 'Sunday', '08:00:00', 2, 1) ,
+		(NULL, 'Sofia-Liylin 2', 'Sunday', '09:30:00', 2, 2) ,	
+		(NULL, 'Sofia-Liylin 3', 'Tuesday', '09:00:00', NULL, NULL) ,			
+		(NULL, 'Plovdiv', 'Monday', '12:00:00', '1', '1');
+		
+INSERT INTO student_sport 
+VALUES 	(1, 1),
+		(2, 1),
+		(3, 1),
+		(4, 2),
+		(5, 2),
+		(6, 2),
+		(1, 3),
+		(2, 3),
+		(3, 3);
+		
+INSERT INTO `school_sport_clubs`.`taxespayments` 
+VALUES	(NULL, '1', '1', '200', '1', 2022, now()),
+		(NULL, '1', '1', '200', '2', 2022, now()),
+		(NULL, '1', '1', '200', '3', 2022, now()),
+		(NULL, '1', '1', '200', '4', 2022, now()),
+		(NULL, '1', '1', '200', '5', 2022, now()),
+		(NULL, '1', '1', '200', '6', 2022, now()),
+		(NULL, '1', '1', '200', '7', 2022, now()),
+		(NULL, '1', '1', '200', '8', 2022, now()),
+		(NULL, '1', '1', '200', '9', 2022, now()),
+		(NULL, '1', '1', '200', '10', 2022, now()),
+		(NULL, '1', '1', '200', '11', 2022, now()),
+		(NULL, '1', '1', '200', '12', 2022, now()),
+		(NULL, '2', '1', '250', '1', 2022, now()),
+		(NULL, '2', '1', '250', '2', 2022, now()),
+		(NULL, '2', '1', '250', '3', 2022, now()),
+		(NULL, '2', '1', '250', '4', 2022, now()),
+		(NULL, '2', '1', '250', '5', 2022, now()),
+		(NULL, '2', '1', '250', '6', 2022, now()),
+		(NULL, '2', '1', '250', '7', 2022, now()),
+		(NULL, '2', '1', '250', '8', 2022, now()),
+		(NULL, '2', '1', '250', '9', 2022, now()),
+		(NULL, '2', '1', '250', '10', 2022, now()),
+		(NULL, '2', '1', '250', '11', 2022, now()),
+		(NULL, '2', '1', '250', '12', 2022, now()),
+		(NULL, '3', '1', '250', '1', 2022, now()),
+		(NULL, '3', '1', '250', '2', 2022, now()),
+		(NULL, '3', '1', '250', '3', 2022, now()),
+		(NULL, '3', '1', '250', '4', 2022, now()),
+		(NULL, '3', '1', '250', '5', 2022, now()),
+		(NULL, '3', '1', '250', '6', 2022, now()),
+		(NULL, '3', '1', '250', '7', 2022, now()),
+		(NULL, '3', '1', '250', '8', 2022, now()),
+		(NULL, '3', '1', '250', '9', 2022, now()),
+		(NULL, '3', '1', '250', '10', 2022, now()),
+		(NULL, '3', '1', '250', '11', 2022, now()),
+		(NULL, '3', '1', '250', '12', 2022, now()),
+		(NULL, '1', '2', '200', '1', 2022, now()),
+		(NULL, '1', '2', '200', '2', 2022, now()),
+		(NULL, '1', '2', '200', '3', 2022, now()),
+		(NULL, '1', '2', '200', '4', 2022, now()),
+		(NULL, '1', '2', '200', '5', 2022, now()),
+		(NULL, '1', '2', '200', '6', 2022, now()),
+		(NULL, '1', '2', '200', '7', 2022, now()),
+		(NULL, '1', '2', '200', '8', 2022, now()),
+		(NULL, '1', '2', '200', '9', 2022, now()),
+		(NULL, '1', '2', '200', '10', 2022, now()),
+		(NULL, '1', '2', '200', '11', 2022, now()),
+		(NULL, '1', '2', '200', '12', 2022, now()),
+		(NULL, '4', '2', '200', '1', 2022, now()),
+		(NULL, '4', '2', '200', '2', 2022, now()),
+		(NULL, '4', '2', '200', '3', 2022, now()),
+		(NULL, '4', '2', '200', '4', 2022, now()),
+		(NULL, '4', '2', '200', '5', 2022, now()),
+		(NULL, '4', '2', '200', '6', 2022, now()),
+		(NULL, '4', '2', '200', '7', 2022, now()),
+		(NULL, '4', '2', '200', '8', 2022, now()),
+		(NULL, '4', '2', '200', '9', 2022, now()),
+		(NULL, '4', '2', '200', '10', 2022, now()),
+		(NULL, '4', '2', '200', '11', 2022, now()),
+		(NULL, '4', '2', '200', '12', 2022, now()),
+		/**2021**/
+		(NULL, '1', '1', '200', '1', 2021, now()),
+		(NULL, '1', '1', '200', '2', 2021, now()),
+		(NULL, '1', '1', '200', '3', 2021, now()),
+		(NULL, '1', '1', '200', '4', 2021, now()),
+		(NULL, '1', '1', '200', '5', 2021, now()),
+		(NULL, '1', '1', '200', '6', 2021, now()),
+		(NULL, '1', '1', '200', '7', 2021, now()),
+		(NULL, '1', '1', '200', '8', 2021, now()),
+		(NULL, '1', '1', '200', '9', 2021, now()),
+		(NULL, '1', '1', '200', '10', 2021, now()),
+		(NULL, '1', '1', '200', '11', 2021, now()),
+		(NULL, '1', '1', '200', '12', 2021, now()),
+		(NULL, '2', '1', '250', '1', 2021, now()),
+		(NULL, '2', '1', '250', '2', 2021, now()),
+		(NULL, '2', '1', '250', '3', 2021, now()),
+		(NULL, '2', '1', '250', '4', 2021, now()),
+		(NULL, '2', '1', '250', '5', 2021, now()),
+		(NULL, '2', '1', '250', '6', 2021, now()),
+		(NULL, '2', '1', '250', '7', 2021, now()),
+		(NULL, '2', '1', '250', '8', 2021, now()),
+		(NULL, '2', '1', '250', '9', 2021, now()),
+		(NULL, '2', '1', '250', '10', 2021, now()),
+		(NULL, '2', '1', '250', '11', 2021, now()),
+		(NULL, '2', '1', '250', '12', 2021, now()),
+		(NULL, '3', '1', '250', '1', 2021, now()),
+		(NULL, '3', '1', '250', '2', 2021, now()),
+		(NULL, '3', '1', '250', '3', 2021, now()),
+		(NULL, '3', '1', '250', '4', 2021, now()),
+		(NULL, '3', '1', '250', '5', 2021, now()),
+		(NULL, '3', '1', '250', '6', 2021, now()),
+		(NULL, '3', '1', '250', '7', 2021, now()),
+		(NULL, '3', '1', '250', '8', 2021, now()),
+		(NULL, '3', '1', '250', '9', 2021, now()),
+		(NULL, '3', '1', '250', '10', 2021, now()),
+		(NULL, '3', '1', '250', '11', 2021, now()),
+		(NULL, '3', '1', '250', '12', 2021, now()),
+		(NULL, '1', '2', '200', '1', 2021, now()),
+		(NULL, '1', '2', '200', '2', 2021, now()),
+		(NULL, '1', '2', '200', '3', 2021, now()),
+		(NULL, '1', '2', '200', '4', 2021, now()),
+		(NULL, '1', '2', '200', '5', 2021, now()),
+		(NULL, '1', '2', '200', '6', 2021, now()),
+		(NULL, '1', '2', '200', '7', 2021, now()),
+		(NULL, '1', '2', '200', '8', 2021, now()),
+		(NULL, '1', '2', '200', '9', 2021, now()),
+		(NULL, '1', '2', '200', '10', 2021, now()),
+		(NULL, '1', '2', '200', '11', 2021, now()),
+		(NULL, '1', '2', '200', '12', 2021, now()),
+		(NULL, '4', '2', '200', '1', 2021, now()),
+		(NULL, '4', '2', '200', '2', 2021, now()),
+		(NULL, '4', '2', '200', '3', 2021, now()),
+		(NULL, '4', '2', '200', '4', 2021, now()),
+		(NULL, '4', '2', '200', '5', 2021, now()),
+		(NULL, '4', '2', '200', '6', 2021, now()),
+		(NULL, '4', '2', '200', '7', 2021, now()),
+		(NULL, '4', '2', '200', '8', 2021, now()),
+		(NULL, '4', '2', '200', '9', 2021, now()),
+		(NULL, '4', '2', '200', '10', 2021, now()),
+		(NULL, '4', '2', '200', '11', 2021, now()),
+		(NULL, '4', '2', '200', '12', 2021, now()),
+		/**2020**/
+		(NULL, '1', '1', '200', '1', 2020, now()),
+		(NULL, '1', '1', '200', '2', 2020, now()),
+		(NULL, '1', '1', '200', '3', 2020, now()),
+		(NULL, '2', '1', '250', '1', 2020, now()),
+		(NULL, '3', '1', '250', '1', 2020, now()),
+		(NULL, '3', '1', '250', '2', 2020, now()),
+		(NULL, '1', '2', '200', '1', 2020, now()),
+		(NULL, '1', '2', '200', '2', 2020, now()),
+		(NULL, '1', '2', '200', '3', 2020, now()),
+		(NULL, '4', '2', '200', '1', 2020, now()),
+		(NULL, '4', '2', '200', '2', 2020, now());
+
+insert into `school_sport_clubs`.students (name, egn, address, class, phone)
+values("Ivan Ivanov Ivanov", "9207186373", "Sofia-Serdika", "10", "0888892950");
+
+select name, 
+    egn,
+    address,
+    class,
+    phone
+  from students
+  order by name asc;
+  
+delete from `school_sport_clubs`.students 
+where id =13;
+
+select students.name, sp.name from students Join student_sport ss On students.id = ss.student_id
+	Join sportgroups sg ON ss.sportGroup_id = sg.id
+		join sports sp on sg.sport_id = sp.id;
+
+SELECT students.name, students.class, sportGroups.id
+from students join sportGroups
+ON students.id IN(
+    SELECT student_id
+    from student_sport
+    WHERE student_sport.sportGroup_id = sport_id
+    )
+WHERE sportGroups.dayOfWeek = 'Sunday';
+
+SELECT coaches.name
+from coaches join sportGroups
+ON coaches.id IN(
+    SELECT coach_id
+    from sportGroups
+    WHERE sportGroups.coach_id = coaches.id
+    )
+WHERE sportGroups.sport_id = 1;
+
+SELECT sportGroups.location, sportGroups.hourOfTraining, sportGroups.dayOfWeek
+from sportGroups join sports
+ON sportGroups.id IN(
+    SELECT sport_id
+    from sportGroups
+    WHERE sportGroups.sport_id = sports.id
+    )
+WHERE sportGroups.sport_id = 2;
+
+SELECT sports.name
+from students JOIN sports
+ON students.id IN(
+    SELECT student_id
+    FROM student_sport
+    WHERE student_sport.sportGroup_id = sports.id
+    )
+WHERE students.name = 'Iliyan Ivanov';
+
+SELECT students.name
+from students join sportGroups
+ON students.id IN(
+    SELECT student_id
+    from student_sport
+    WHERE student_sport.sportGroup_id = sport_id
+    )
+WHERE sportGroups.coach_id = 1;
+
+
